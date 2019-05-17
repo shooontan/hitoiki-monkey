260ec1eec1683eb81fe5dd99913f242389296d16
shooontan <logo.bee.doo19@gmail.com>
2019-05-17 11:04:35 +0900

    fix comment layout

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -2,13 +2,24 @@ import { h } from 'hyperapp';
 
 export default function Comment(props) {
   const { status, transcript, transcripts } = props;
+
+  // TODO
+  const listElm = document.getElementById('comment-list');
+  if (listElm) {
+    listElm.scrollTop = listElm.scrollHeight;
+  }
+
   return (
     <div class="comment">
       <p>{status}</p>
       <p>{transcript}</p>
-      <ul>
+      <ul id="comment-list" class="comment-list">
         {transcripts.map(_transcript => (
-          <li>{_transcript}</li>
+          <li class="comment-list-item">
+            <div>
+              <span>{_transcript}</span>
+            </div>
+          </li>
         ))}
       </ul>
     </div>
